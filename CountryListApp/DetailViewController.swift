3c37e44ccd3d1c20b83669ab19608e2bff4adb9a
OnurEmren <developeronur1@gmail.com>
2023-03-22 07:48:39 +0300

    share country

--- CountryListApp/DetailViewController.swift
+++ CountryListApp/DetailViewController.swift
@@ -17,14 +17,32 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = imageArray.title.uppercased()
+        
         detailImageView.image = UIImage(named: imageArray.imageName)
-        detailNameLabel.text = imageArray.title.uppercased()
-        detailImageView.layer.borderWidth = 0.5
+        //detailNameLabel.text = imageArray.title.uppercased()
+        detailImageView.layer.borderWidth = 2
         navigationItem.largeTitleDisplayMode = .never
         
-     
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
         
+    }
+    
+    @objc func shareTapped(){
+        
+        guard let image = detailImageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No items")
+            
+            return
+        }
+        
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
--- CountryListApp/ViewController.swift
+++ CountryListApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     
     @IBOutlet weak var listTableView: UITableView!
     
-    
+    //List
     let imageArray = [ImageArray(title: "estonia", imageName: "estonia"),
                       ImageArray(title: "france", imageName: "france"),
                       ImageArray(title: "germany", imageName: "germany"),
@@ -28,7 +28,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
                       ImageArray(title: "us", imageName: "us"),
     ]
     
-    
+    //Image List
     private var countriesImage = [UIImage(named: "estonia"),
                                   UIImage(named: "france"),
                                   UIImage(named: "germany"),
@@ -48,15 +48,10 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         super.viewDidLoad()
         
         
-        
         title = "Countries"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        
-        
         listTableView.reloadData()
-        
-        //listTableView.register(MyTableViewCell.self, forCellReuseIdentifier: "MySecondCell")
         listTableView.delegate = self
         listTableView.dataSource = self
         
@@ -64,6 +59,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         
     }
     
+    //TableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return imageArray.count
     }
@@ -85,10 +81,13 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         
     }
     
+    //Row Views
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
     
+    
+    //Navigation
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let myList = imageArray[indexPath.row]
         performSegue(withIdentifier: "goToDetail", sender: myList)
